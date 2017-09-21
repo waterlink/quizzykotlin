2c6875d1b59ff33af2084bd88e6a9d1d3ecf0406
Oleksii Fedorov <waterlink000@gmail.com>
2017-09-21 22:31:31 +0200

    Flesh out the first acceptance test

---
+++ src/test/kotlin/business/`Oliver receives one question at a time - acceptance test`.kt
@@ -0,0 +1,29 @@
+package business
+
+import org.junit.Assert
+import org.junit.Test
+import java.util.*
+
+class `Oliver receives one question at a time - acceptance test` {
+
+    @Test
+    fun `there should be one current question when quiz starts`() {
+
+        // Given there are questions in the quiz
+        val first = Question(
+                id = UUID.randomUUID().toString(),
+                title = "How to create a method with spaces in Kotlin?")
+        val second = Question(
+                id = UUID.randomUUID().toString(),
+                title = "How to create JUnit4 test function in Kotlin?")
+        val quiz = Quiz(questions = listOf(first, second))
+
+        // When I start the quiz
+        quiz.start()
+
+        // Then I see only one question from that quiz
+        val currentQuestion = quiz.currentQuestion
+        Assert.assertTrue(listOf(first, second).contains(currentQuestion))
+
+    }
+}
