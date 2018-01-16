3934e9ca80f90b2cce77f6f40b4085d668b28207
Oleksii Fedorov <waterlink000@gmail.com>
2018-01-16 07:45:58 +0100

    Write acceptance unit test for choosing an answer option by index

--- src/main/kotlin/business/Question.kt
+++ src/main/kotlin/business/Question.kt
@@ -2,4 +2,8 @@ package business
 
 data class Question(val id: String,
                     val title: String,
-                    val answerOptions: List<AnswerOption>)
+                    val answerOptions: List<AnswerOption>) {
+    fun chooseAnswerOption(index: Int) {
+
+    }
+}
--- src/test/kotlin/business/Oliver can choose an answer option – acceptance test.kt
+++ src/test/kotlin/business/Oliver can choose an answer option – acceptance test.kt
@@ -38,4 +38,42 @@ class `Oliver can choose an answer option – acceptance test` {
         assertEquals(false, currentQuestion.answerOptions[0].isChosen)
         assertEquals(false, currentQuestion.answerOptions[1].isChosen)
     }
+
+    @Test
+    fun `choosing an answer option by index`() {
+        // Given there is a quiz with a question
+        //      with a few answer options
+        val answerOne = AnswerOption(
+                id = UUID.randomUUID().toString(),
+                title = "Multiline string")
+        val answerTwo = AnswerOption(
+                id = UUID.randomUUID().toString(),
+                title = "Shorthand for list of strings")
+
+        val question = Question(
+                id = UUID.randomUUID().toString(),
+                title = "What is raw string literal for?",
+                answerOptions = listOf(
+                        answerOne,
+                        answerTwo))
+
+        val quiz = Quiz(
+                id = UUID.randomUUID().toString(),
+                questions = listOf(question))
+
+        // And I have already started the quiz
+        quiz.start()
+
+        // And I have already navigated to that question
+        val currentQuestion = quiz.currentQuestion!!
+
+        // When I select an answer option
+        currentQuestion.chooseAnswerOption(0)
+
+        // Then I see it is selected
+        assertEquals(true, currentQuestion.answerOptions[0].isChosen)
+
+        // And I see no other option is selected
+        assertEquals(false, currentQuestion.answerOptions[1].isChosen)
+    }
 }
