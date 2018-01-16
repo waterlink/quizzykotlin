91d4f9db75505f98180b10ab6c1dabf19824d984
Oleksii Fedorov <waterlink000@gmail.com>
2018-01-16 12:49:33 +0100

    Make the 'choosing after it was already chosen' test pass

--- src/main/kotlin/business/Question.kt
+++ src/main/kotlin/business/Question.kt
@@ -4,6 +4,9 @@ data class Question(val id: String,
                     val title: String,
                     val answerOptions: List<AnswerOption>) {
     fun chooseAnswerOption(index: Int) {
+        answerOptions.forEach {
+            it.isChosen = false
+        }
         answerOptions[index].isChosen = true
     }
 }
