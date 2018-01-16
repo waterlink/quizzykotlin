e0ec2808162332a1628054884a166f0d654652e0
Oleksii Fedorov <waterlink000@gmail.com>
2018-01-16 06:58:21 +0100

    Add skeleton for the first acceptance test for the feature of selecting an answer option

---
+++ src/test/kotlin/business/Oliver can choose an answer option – acceptance test.kt
@@ -0,0 +1,15 @@
+package business
+
+import org.junit.Test
+
+class `Oliver can choose an answer option – acceptance test` {
+
+    @Test
+    fun `there is no selected answer option initially`() {
+        // Given there is a quiz with a question
+        //      with a few answer options
+        // When I start the quiz
+        // And navigate to that question
+        // Then I see that no answer option is selected
+    }
+}
