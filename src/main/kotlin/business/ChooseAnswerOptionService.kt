e1f7c600eb88d09ceb684217fa534a7f8edfd295
Oleksii Fedorov <waterlink000@gmail.com>
2018-01-18 07:53:15 +0100

    Add loadOrFail method to the quiz storage and use it from choose answer option service

--- src/main/kotlin/business/ChooseAnswerOptionService.kt
+++ src/main/kotlin/business/ChooseAnswerOptionService.kt
@@ -4,8 +4,7 @@ class ChooseAnswerOptionService(
         private val quizStorage: QuizStorage) {
 
     fun chooseAnswerOption(quizId: String, index: Int): Question {
-        val quiz = quizStorage.load(quizId) ?:
-                throw QuizNotFoundException(quizId)
+        val quiz = quizStorage.loadOrFail(quizId)
 
         val currentQuestion = quiz.currentQuestion ?:
                 throw QuizHasNoQuestionsException(quizId)
--- src/main/kotlin/business/QuizStorage.kt
+++ src/main/kotlin/business/QuizStorage.kt
@@ -4,4 +4,8 @@ interface QuizStorage {
 
     fun load(id: String): Quiz?
 
+    fun loadOrFail(id: String): Quiz {
+        return load(id) ?: throw QuizNotFoundException(id)
+    }
+
 }
